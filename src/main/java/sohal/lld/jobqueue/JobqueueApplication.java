3db89be45559ce8f66219ef05c3caccf815e2313
rahulsohal <rahulsohal.nith@gmail.com>
2025-08-22 03:22:12 +0530

    init commit

---
+++ src/main/java/sohal/lld/jobqueue/JobqueueApplication.java
@@ -0,0 +1,13 @@
+package sohal.lld.jobqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobqueueApplication.class, args);
+	}
+
+}
